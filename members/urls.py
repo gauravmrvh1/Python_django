9cdd41bfb13865b11119d8ce4147f3daa470bcfb
my-system <gauravmrvh1@gmail.com>
2023-02-04 12:37:39 +0530

    up

--- members/urls.py
+++ members/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from . import views
+from . import member_views
 
 urlpatterns = [
-    path('', views.main, name='main'),
-    path('members/', views.members, name='members'),
-    path('members/details/<int:id>', views.details, name='details'),
-    path('testing/', views.testing, name='testing'), 
+    path('', member_views.main, name='main'),
+    path('members/', member_views.members, name='members'),
+    path('members/details/<int:id>', member_views.details, name='details'),
+    path('testing/', member_views.testing, name='testing'), 
 ]
